8f3d3f9f9244f74f205fe524dfa6b6b60d4b3ae0
ErnanyGoncalves <39605915+ErnanyGoncalves@users.noreply.github.com>
2021-03-15 18:29:13 -0300

    Exclusão, ordenação e estilização da tabela de times

--- src/MyTeams.js
+++ src/MyTeams.js
@@ -5,14 +5,34 @@ import { TeamContext } from './TeamContext';
 import { ReactComponent as Pencil } from "./Icons/edit.svg"
 import { ReactComponent as Trash } from "./Icons/delete.svg"
 import { ReactComponent as Share } from "./Icons/share.svg"
+import { ReactComponent as UpDown } from "./Icons/up-down.svg"
 
 const MyTeams = () => {
     const { team, setTeam } = React.useContext(TeamContext);
     const deleteTeam = (id) => {
         const confirmation = window.confirm(`Are you sure you want to delete ${team[id].name} team?`);
         if (confirmation) {
-            setTeam(team.splice(id, 1));
+            team.splice(id, 1);
+            setTeam([...team]);
         }
+    }
+
+    const sortByName = () => {
+        const sortedTeams = [...team].sort((a, b) => {
+            if (a.name < b.name) return -1;
+            if (a.name > b.name) return 1;
+            return 0;
+        });
+        setTeam(sortedTeams);
+    }
+
+    const sortByDesc = () => {
+        const sortedTeams = [...team].sort((a, b) => {
+            if (a.description < b.description) return -1;
+            if (a.description > b.description) return 1;
+            return 0;
+        });
+        setTeam(sortedTeams);
     }
 
     return (
@@ -27,27 +47,36 @@ const MyTeams = () => {
 
             {team.length === 0 ?
                 (<div className={styles.noTeams}>
-                    <p>You have no teams. Would you like to create your first one?</p>
+                    <p>You have no teams. Would you like to create the first one?</p>
                 </div>)
                 :
                 (<div className={styles.table}>
                     <div className={styles.tableHeader}>
-                        <p className={styles.teamNameHeader}>Name</p>
-                        <p className={styles.descHeader}>Description</p>
+                        <div className={styles.column1} onClick={sortByName}>
+                            <p className={styles.teamNameHeader}>Name</p>
+                            <UpDown className={styles.iconH} />
+                        </div>
+                        <div className={styles.column2} onClick={sortByDesc}>
+                            <p className={styles.descHeader}>Description</p>
+                            <UpDown className={styles.iconH} />
+                        </div>
                     </div>
+
 
                     {team.map((team, id) => (
                         <div className={styles.tableBody} key={id}>
                             <p className={styles.teamName}>{team.name}</p>
                             <p className={styles.desc}>{team.description}</p>
                             <div className={styles.controllers}>
-                                <Trash onClick={() => deleteTeam(id)} className={styles.icon} />
+                                <Trash onClick={() => deleteTeam(id, team)} className={styles.icon} />
                                 <Share className={styles.icon} />
                                 <Link to={`/edit/${id}`}>
                                     <Pencil className={styles.icon} />
                                 </Link>
                             </div>
+
                         </div>
+
                     ))}
                 </div>)
             }
